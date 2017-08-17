ff9da14e7700ea73d796d4c5750396e90e295d31
Chris Kim <gyjkim09@gmail.com>
2017-08-17 15:57:49 -0700

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  In Programming Assignment 2, We use the lexical scoping to 
+##  cache the matrix inverse if it is already calculated and is being
+##  used second time.
 
-## Write a short comment describing this function
+## makeCacheMatrix defines two getter and two setter functions using
+## the concept of lexical scoping
+## four functions are returned as a list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve checks if inv exists, if so it pulls from the cache 
+## in the memory, if not it calculates and set the inverse in the 
+## memory
+## return value is the inverse matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
